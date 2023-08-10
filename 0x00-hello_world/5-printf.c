d20c5b3d4b9c714085a5a5075655780ccef26a80
ACHUX21 <1337.achraf@gmail.com>
2023-08-10 13:09:12 -0700

    My first Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(){
+/**
+*main - Printf First Function
+*/
+
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
