2cd7273c155bc61b81236970a4668f38a6df1af6
ACHUX21 <1337.achraf@gmail.com>
2023-08-11 13:02:22 -0700

    printing (num | rev)

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+		for (i = 122 ; i > 96 ; i--)
+		{
+			putchar(i);
+		}
+	putchar('\n');
+	return (0);
+}
