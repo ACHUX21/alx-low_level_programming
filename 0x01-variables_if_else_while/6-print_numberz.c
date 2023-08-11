0ce89b49e8c014962a4001dfecfc3310704ce236
ACHUX21 <1337.achraf@gmail.com>
2023-08-11 12:58:47 -0700

    Printing Numbers using putchar without char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+		for (i = 48 ; i <= 48 + 9 ; i++)
+		{
+			putchar(i);
+		}
+	putchar('\n');
+	return (0);
+}
