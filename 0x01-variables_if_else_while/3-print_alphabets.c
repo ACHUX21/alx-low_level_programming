db9e1f3a48408939345c19b2ee09f6cea6abb352
ACHUX21 <1337.achraf@gmail.com>
2023-08-11 12:37:21 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, j;
+
+		for (i = 97 ; i < 123 ; i++)
+		{
+			putchar(i);
+		}
+
+		for (j = 65; j < 91 ; j++)
+		{
+			pytchar(j);
+		}
+
+	putchar('\n');
+	return (0);
+
+}
