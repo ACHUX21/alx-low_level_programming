54350de0c14bc0ffee0ab71f7f508de002df68a8
ACHUX21 <1337.achraf@gmail.com>
2023-08-11 12:23:58 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,15 +25,15 @@ int main(void)
 
 	if (l > 5)
 	{
-	printf("and is greater than 5");
+	printf("and is greater than 5\n");
 	}
 	else if (l < 6 && l != 0)
 	{
-	printf("and is less than 6 and not 0");
+	printf("and is less than 6 and not 0\n");
 	}
 	else
 	{
-	printf("and is 0");
+	printf("and is 0\n");
 	}
 
 	return (0);
