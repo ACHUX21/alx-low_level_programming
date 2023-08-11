137b71f8183732218429df6b446853458b2dfe60
ACHUX21 <1337.achraf@gmail.com>
2023-08-11 15:30:36 -0700

    hackor

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,23 @@
 
 int main(void)
 {
-		int i, j;
-		int k = 0;
+	int i, j;
+	int k = 0;
 
-		for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0; j < 10 ; j++)
 		{
-			for (j = 0; j < 10 ; j++)
+			if (i != '9' && j != '9')
 			{
-				if (i != '9' && j != '9')
-				{
-					if (k % 2 != 0)
-						putchar(',');
-					putchar(' ');
-				}
-				putchar(i + 48);
-				putchar(j + 48);
-			k++;
+				if (k % 2 != 0)
+					putchar(',');
+				putchar(' ');
 			}
+			putchar(i + 48);
+			putchar(j + 48);
+		k++;
 		}
+	}
+return (0);
 }
