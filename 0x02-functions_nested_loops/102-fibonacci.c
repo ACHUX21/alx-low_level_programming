8c7e09e6b80042fc240a7080c260c8403a002dfc
ACHUX21 <1337.achraf@gmail.com>
2023-08-15 15:56:39 -0700

    12

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *		separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long long a = 1, b = 2;
+	int i;
+
+	printf("%llu, %llu", a, b);
+
+	for (i = 2; i < 50; i++)
+	{
+		unsigned long long next = a + b;
+
+		printf(", %llu", next);
+
+		a = b;
+		b = next;
+
+	}
+
+	printf("\n");
+
+	return (0);
+}
