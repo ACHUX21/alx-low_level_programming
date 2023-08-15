649d9e12d9ffeeac509676cb494c56e386154f55
ACHUX21 <1337.achraf@gmail.com>
2023-08-15 15:16:48 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,10 +10,11 @@ int main(void)
 	int i;
 	int sum = 0;
 
-for (i = 0 ; i < 1024 ; i++)
-{
-if (i % 5 == 0 || i % 3 == 0)
-sum += i;
-}
-printf("%d\n", sum);
+	for (i = 0 ; i < 1024 ; i++)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
