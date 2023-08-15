710c5aa9ea6375e0dfbb5eb4def2d6e39363e071
ACHUX21 <1337.achraf@gmail.com>
2023-08-15 13:16:33 -0700

    we hope this is the last commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,38 +8,38 @@
 
 void print_times_table(int n)
 {
-	int x, y, p;
+int x, y, p;
 
-	for (x = 0 ; x <= n ; x++)
-	{
-		for (y = 0 ; y <= n ; y++)
-		{
-			p = x * y;
-			if (p >= 10 && p < 100)
-			{
-				_putchar(p / 10 + 48);
-				_putchar(p % 10 + 48);
-			}
-			else if (p >= 100)
-			{
-				_putchar(p / 100 + 48);
-				_putchar(p / 10 % 10 + 48);
-				_putchar(p % 10 + 48);
-			}
-			else
-			{
-				_putchar(p + 48);
-			}
-			if (y != n)
-			{
-			_putchar(',');
-				if (p < 10)
-					_putchar(' ');
-				if (p < 100)
-					_putchar(' ');
-			_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+for (x = 0 ; x <= n ; x++)
+{
+for (y = 0 ; y <= n ; y++)
+{
+if (y != n + 1 && y != 0)
+{
+putchar(',');
+if (p < 10)
+putchar(' ');
+if (p < 100)
+putchar(' ');
+putchar(' ');
+}
+p = x * y;
+if (p >= 10 && p < 100)
+{
+_putchar(p / 10 + 48);
+_putchar(p % 10 + 48);
+}
+else if (p >= 100)
+{
+_putchar(p / 100 + 48);
+_putchar(p / 10 % 10 + 48);
+_putchar(p % 10 + 48);
+}
+else
+{
+_putchar(p + 48);
+}
+}
+_putchar('\n');
+}
 }
