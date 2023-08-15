fed500aaefe1bd68cab16796235cbdacb64f675c
ACHUX21 <1337.achraf@gmail.com>
2023-08-14 22:54:21 -0700

    Commiting Task 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Entry point
+* Description: a function that prints the alphabets 10 times,
+* in lowecase followed by a new line
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char n;
+	int k = 0;
+
+	while (k <= 10)
+	{
+		for (n = 'a' ; n <= 'z' ; n++)
+		{
+			_putchar(n);
+		}
+
+	_putchar('\n');
+	k++;
+
+	}
+	_putchar('\n');
+}
