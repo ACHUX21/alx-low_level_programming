42e3d42d7848c03c3df373f027ac07a0f5cfcf82
ACHUX21 <achux@localhost.localdomain>
2023-08-17 13:01:36 +0100

    p

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - Prints an integer
+ * @n: The integer to be printed
  */
+
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int num;
 
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
-	}
 
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
+		num = -n;
 	}
-
-	_putchar((n1 % 10) + '0');
+	else
+		num = n;
+	if (num / 10)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
