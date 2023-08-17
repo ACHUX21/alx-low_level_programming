fbfd095cb6e5b7de92b6ad6cde3144deaa39bdea
ACHUX21 <achux@localhost.localdomain>
2023-08-17 12:48:37 +0100

    p

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,25 @@
-#include "mainh"
+#include "main.h"
+/**
+ * print_triangle - Function
+ *
+ * @size: Integer
+ */
+
+void print_triangle(int size)
+{
+int i, j, k;
+
+for (i = size ; i >= 0 ; i--)
+{
+for (j = i ; j > -1 ; j--)
+{
+putchar(' ');
+}
+for (k = i ; k <= size ; k++)
+{
+putchar('#');
+}
+
+putchar('\n');
+}
+}
