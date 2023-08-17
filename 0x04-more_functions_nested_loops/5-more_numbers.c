ae0c1f7b766bcd88793035b57fa80ba06f87869f
ACHUX21 <1337.achraf@gmail.com>
2023-08-17 03:03:33 -0700

    Numbers Task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,11 +5,13 @@
 void more_numbers(void)
 {
 	int i, j;
-
+for (j = 0 ; j < 10 ; j++)
+{
 	for (i = 0 ; i < 15 ; i++)
 	{
 		if (i > 9)
 			_putchar(i / 10 + 48);
 		_putchar(i % 10 + 48);
 	}
+}
 }
