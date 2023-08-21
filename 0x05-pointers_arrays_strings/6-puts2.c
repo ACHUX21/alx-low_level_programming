ec4ccd31ab5161521abd15bbcc2edd6c390b6372
ACHUX21 <achux@localhost.localdomain>
2023-08-21 14:27:46 +0100

    Task 7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,14 @@
 void puts2(char *str)
 {
 	int i = 0;
+    int lenght = 0;
 
-	while (str[i] != '\0')
+    while (s[lenght] != '\0')
+        lenght++;
+
+	for (i = 0; i < lenght - 1; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
 	_putchar('\n');
 }
