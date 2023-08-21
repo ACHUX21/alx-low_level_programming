d8df7a9ff999f9d379f256af3bcdffe1119b6ecf
ACHUX21 <achux@localhost.localdomain>
2023-08-21 22:02:43 +0100

    Final shit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 /**
  * @brief 
  * 
