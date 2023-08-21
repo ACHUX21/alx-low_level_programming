bf6ba84f3b6882c1b0601d7f47876d8896fbf39f
ACHUX21 <achux@localhost.localdomain>
2023-08-21 14:12:29 +0100

    Task 6

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
 
 void rev_string(char *s)
 {
-	int length = 0;
+	int i, length = 0;
 
 	while (s[length] != '\0')
 		length++;
@@ -15,7 +15,7 @@ void rev_string(char *s)
 	char rev[length + 1];
 
 
-	for (int i = 0; i < length; i++)
+	for (i = 0; i < length; i++)
 	{
 		rev[i] = s[length - 1 - i];
 	}
