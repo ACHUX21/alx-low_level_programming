eea1a7e9d33be3a474f3531be2543c92341ef7d2
ACHUX21 <achux@localhost.localdomain>
2023-08-21 23:51:20 +0100

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,45 +1,46 @@
 #include "main.h"
-
 /**
  * _atoi - Convert a string to an integer
  * @s: String to convert
  * Return: integer
  */
-
 int _atoi(char *s)
 {
-int i = 0;
-int negative = 0,
-unsigned int h;
-int check = 0;
+	int i = 0;
+	int negative = 0;
+	unsigned int h;
+	int check = 0;
 
-while (s[i] != '\0')
-{
-if (check == 1 && (s[i] < '0' || s[i] > '9'))
-{
-break;
-}
+	while (s[i] != '\0')
+	{
+		if (check == 1 && (s[i] < '0' || s[i] > '9'))
+		{
+			break;
+		}
 
-if (s[i] == 32 || s[i] == 9)
-{
+		if (s[i] == 32 || s[i] == 9)
+		{
+			i++;
+			continue;
+		}
+
+		if (s[i] == '-')
+			negative++;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			h = h * 10 + s[i] - 48;
+			check = 1;
+		}
 i++;
-continue;
-}
-if (s[i] == '-')
-negative++;
-if (s[i] >= '0' && s[i] <= '9')
-{
-h = h * 10 + s[i] - 48;
-check = 1;
-}
-i++;
-}
-if (negative % 2 == 0)
-{
-return (h);
-}
-else
-{
-return (-h);
-}
+	}
+
+	if (negative % 2 == 0)
+	{
+		return (h);
+	}
+	else
+	{
+		return (-h);
+	}
 }
