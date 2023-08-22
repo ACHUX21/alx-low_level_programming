3b7a389c444e4661cfdd8602478c4d576083716d
root <root@localhost.localdomain>
2023-08-22 11:54:05 +0100

    pushing

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,18 @@
 
 void rev_string(char *s)
 {
-	char reverse = s[0];
-	int lenght = 0;
-	int i;
+char reverse = s[0];
+int lenght = 0;
+int i;
 
-	while (s[lenght] != '\0')
-	    lenght++;
+while (s[lenght] != '\0')
+lenght++;
 
-    for (i = 0; i < lenght; i++)
-	{
-		lenght--;
-		reverse = s[i];
-		s[i] = s[lenght];
-		s[lenght] = reverse;
-	}
+for (i = 0; i < lenght; i++)
+{
+lenght--;
+reverse = s[i];
+s[i] = s[lenght];
+s[lenght] = reverse;
+}
 }
