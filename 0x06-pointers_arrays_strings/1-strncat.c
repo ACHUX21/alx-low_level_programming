409dd95cd121d7d0c87edeca3ecb02d5aa635697
root <root@localhost.localdomain>
2023-08-23 20:21:30 +0100

    p

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,41 +4,19 @@
  * @dest: Pointer to the destination string.
  * @src: Pointer to the source string.
  * @n: The maximum number of bytes to be copied from src.
- *
- * Description:
- * This function appends at most n bytes from the source string (src)
- * to the end of the destination string (dest). If src contains n or
- * more bytes, it does not need to be null-terminated.
- *
- * Return:
- * Pointer to the resulting destination string (dest).
+ * Return: Pointer
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-	int length1, length2;
+	int i	= 0, j = 0;
 
-	length1 = 0;
-	length2 = 0;
-	while (src[length1] != '\0')
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
 	{
-		length1++;
-	}
-	while (dest[length2] != '\0')
-	{
-		length2++;
-	}
-
-	for (i = length2; i < length1 + length2; i++)
-	{
-		for (j = 0; j < length1; j++)
-		{
-			if (j < n)
-			{
-				dest[i] = src[j];
-			}
-			i++;
-		}
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
