9b514cc14f4f93fda65b3a1f8cab670a7b5e2dd0
ACHUX21 <achux@localhost.localdomain>
2023-08-24 09:50:03 +0100

    Hello

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * cap_string - Function
- * @str: String to be capitalized
+ * @s: String to be capitalized
  * Return: Capitalized string
  *
  */
 char *cap_string(char *s)
 {
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
-	'!', '?', '"', '(', ')', '{', '}'};
-	int i, j;
+int i, j;
 
-	i = 0;
-	if (s[i] >= 'a' && s[i] <= 'z')
-	{
-		s[i] -=  32;
-	}
-	while (s[i])
-	{
-		for (j = 0; j < 13; j++)
-		{
-			if (s[i] == spe[j])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					s[i + 1] -= 32;
-			}
-		}
-		i++;
-	}
-	return (s);
+char spe[13] = {' ', '\t', '\n', ',', ';', '.'
+	, '!', '?', '"', '(', ')', '{', '}'};
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+s[i] -= 32;
+
+for (j = 0; j < 13; j++)
+{
+if (s[i] == spe[j])
+{
+if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+{
+s[i + 1] -= 32;
+}
+}
+}
+}
+
+return (s);
 }
