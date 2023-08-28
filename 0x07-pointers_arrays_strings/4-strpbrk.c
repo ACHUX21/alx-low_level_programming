063cc55a817371436c716d6c39dc601880c16f2a
root <root@localhost.localdomain>
2023-08-28 11:54:47 +0100

    TASK4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,14 +9,13 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
-	while (*s)
+	while (*s++)
 	{
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
 				return (s);
 		}
-		s++;
 	}
 
 	return (s);
