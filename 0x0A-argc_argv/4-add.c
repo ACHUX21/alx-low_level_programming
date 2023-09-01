300e02289ea85d0a92caf0c72d05b5c44802c03f
root <root@localhost.localdomain>
2023-09-01 20:37:38 +0100

    Task4: 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,20 +7,33 @@
 */
 int main(int argc, char *argv[])
 {
-	int i = 0;
-
 	int sum = 0;
+	int i = 1;
+	int j;
 
 	if (argc < 2)
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
+		return (0);
 	}
 
-	for (i = 1; i < argc; i++)
+	while (i < argc)
 	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
 		sum += atoi(argv[i]);
+		i++;
 	}
+
 	printf("%d\n", sum);
 	return (0);
+
 }
