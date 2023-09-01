4f0d3f9c2feab7faddd15bc9717981055e3e8979
root <root@localhost.localdomain>
2023-09-01 20:18:14 +0100

    Task2: 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - main function
+* @argc: number of arguments
+* @argv: command line arguments
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+	int in;
+
+	for (in = 1; in < argc; in++)
+	{
+		printf("%s\n", argv[in]);
+	}
+	return (0);
+}
