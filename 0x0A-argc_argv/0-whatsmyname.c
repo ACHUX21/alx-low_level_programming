a85aaeff36434c45ba92b8d594be20023572f3b5
root <root@localhost.localdomain>
2023-09-01 20:18:09 +0100

    Task0: 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+/**
+* main - program that prints its name, followed by a new line.
+* @argv: argument list
+* @argc: number of arguments
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
