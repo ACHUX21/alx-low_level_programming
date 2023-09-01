ce6d2a7e85cdd9332d232a93c0e30945943b811e
root <root@localhost.localdomain>
2023-09-01 20:18:11 +0100

    Task1: 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main function for the application and application
+ * @argc - argument
+ * @argv - argument list
+ * return: int
+ */
+
+int main(int argc, char *argv)
+
+{
+	int len = 0;
+
+	int i;
+
+	for (i = 0; i < argc; i++)
+		len++;
+
+	printf("%d\n", len - 1);
+	return (0);
+}
