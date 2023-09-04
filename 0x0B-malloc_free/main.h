9cd3081ed550b8a0eff3e3756438ececd30acc69
root <root@localhost.localdomain>
2023-09-04 20:05:21 +0100

    requirement

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+char *strncpy(char *dest, const char *src, size_t n);
+
