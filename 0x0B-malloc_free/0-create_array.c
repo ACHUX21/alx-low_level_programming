37730c268ab20b757dfeef8255b912ebe29ea9d6
root <root@localhost.localdomain>
2023-09-04 20:05:36 +0100

    Task0: 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * create_array - create an array
+ * @size: size of the array
+ * @c: character
+ * Return: array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	p = malloc(sizeof(char) * size);
+
+	if (size == 0 || p == NULL)
+		return (NULL);
+
+	for (i = 1; i <= size; i++)
+		p[i] = c;
+
+	return (p);
+}
