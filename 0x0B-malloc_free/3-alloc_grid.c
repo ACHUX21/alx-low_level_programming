9330994ddca46a47b65e78839f2b9c0b0e7fad89
root <root@localhost.localdomain>
2023-09-04 21:03:27 +0100

    Task3: 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,6 +10,8 @@ int **alloc_grid(int width, int height)
 	int **array;
 
 	int j, i;
+    if (width <= 0 || height <= 0)
+        return (NULL);
 
 	array = (int **)malloc(height * sizeof(int *));
 	if (array == NULL)
