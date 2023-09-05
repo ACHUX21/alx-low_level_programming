dd39248ed4d3de6800a91c388f0639d545bb0c31
root <root@localhost.localdomain>
2023-09-05 17:08:58 +0100

    Task101: 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,8 +6,62 @@
 */
 char **strtow(char *str)
 {
-    if (str == NULL || str == "")
-        return (NULL);
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
+	while (*(str + len))
+		len++;
+	words = _count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+
+				if (tmp == NULL)
+					return (NULL);
+
+				while (start < end)
+					*tmp++ = str[start++];
+
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
+/**
+* _count_words - function that calculates the length of a string
+* @str: the string to calculate length of
+* Return: the length
+*/
+int _count_words(char *str)
+{
+	int len = 0, k = 0;
 
+	for (k = 1; str[k] != '\0'; k++)
+	{
+	if (str[k - 1] != ' ' && str[k] == ' ')
+		len++;
+	}
+	return (len);
+}
