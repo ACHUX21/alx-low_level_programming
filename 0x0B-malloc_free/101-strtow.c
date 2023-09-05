c29fc1700ff864f0db1fa7741898cc4162979e0b
root <root@localhost.localdomain>
2023-09-05 15:27:01 +0100

    Task101: 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* strtow - function that splits a string into words.
+* @str: the string to split
+* Return: returns a pointer to an array of strings (words)
+*/
+char **strtow(char *str)
+{
+    if (str == NULL || str == "")
+        return (NULL);
+
+}
+
