1d87248c134a1e911646dfa2672132d28a92915e
root <root@localhost.localdomain>
2023-09-07 21:26:56 +0100

    Task1: 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,10 +25,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[len2] != '\0')
 		len2++;
 
-	if (len2 > n)
+	if (len2 < n)
 		len2 = n;
 
-	str = malloc(sizeof(char) * (len1 + len2 + 1));
+	str = malloc(sizeof(char) * (len1 + n + 1));
 
 		if (str == NULL)
 			return (NULL);
