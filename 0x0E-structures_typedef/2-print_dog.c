e120a1fde03b76537ba68f3fe634145d5d203f88
root <root@localhost.localdomain>
2023-09-11 20:03:13 +0100

    Task2: 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,12 +4,11 @@
 * print_dog - print the dog object
 * @d: the dog object
 */
+
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		return;
-	}
 	if (d->name == NULL && d->owner == NULL)
 	{
 		printf("Name: %s\n", "(nil)");
@@ -18,8 +17,8 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Name: %s\n", (*d).name);
-		printf("Age: %.6f\n", (*d).age);
-		printf("Owned: %s\n", (*d).owner);
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
 }
