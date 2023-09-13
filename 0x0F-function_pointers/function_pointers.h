54828ae8c5706208d7bcbe29c6df7443af7e5298
root <root@localhost.localdomain>
2023-09-13 21:25:37 +0100

    Requirement

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,11 @@
-void print_name(char name, void (f)(char ));
-void array_iterator(intarray, size_t size, void (action)(int));
-int int_index(intarray, int size, int (cmp)(int));
-int (get_op_func(char *s))(int, int);
+#ifndef _HEADER_
+#define _HEADER_
+#include <stdlib.h>
+
+
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
