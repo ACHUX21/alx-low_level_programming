c8648eefdb0ca3d316d6827316fd2d7b2c1769d9
root <root@localhost.localdomain>
2023-09-13 23:03:46 +0100

    Task3: 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+/**
+* op_add - add a & b
+* @a: number1
+* @b: number2
+* Return: sum
+*/
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+/**
+ * op_sub - subtract a
+ * @a: number1
+ * @b: number2
+ * Return: sum
+*/
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+/**
+ * op_mul - multiply a
+ * @a: number1
+ * @b: number2
+ * Return: sum
+*/
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+/**
+ * op_div - divide a
+ * @a: number1
+ * @b: number2
+ * Return: sum
+*/
+int op_div(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    return (a / b);
+}
+/**
+ * op_mod - mod
+ * @a: number1
+ * @b: number2
+ * Return: sum
+*/
+int op_mod(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    return (a % b);
+}
