f78346df26edf898facdced8c6d0012143ea7b42
root <root@localhost.localdomain>
2023-09-13 21:31:19 +0100

    Task1: 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - iterator function for array elements
+ * @array: pointer
+ * @size: size of the array
+ * @action: function
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    size_t l = 0;
+    if (array != NULL && size != 0 && action != NULL)
+    {
+        while (l < size)
+        {
+            action(array[l]);
+            l++;
+        }
+    }
+
+}
