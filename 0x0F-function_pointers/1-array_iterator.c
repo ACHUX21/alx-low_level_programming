b8378cb23ca1661afb5cafa63007e7600bd4f2a6
root <root@localhost.localdomain>
2023-09-13 21:32:02 +0100

    Task1: 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,14 +7,14 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t l = 0;
-    if (array != NULL && size != 0 && action != NULL)
-    {
-        while (l < size)
-        {
-            action(array[l]);
-            l++;
-        }
-    }
+	size_t l = 0;
+	if (array != NULL && size != 0 && action != NULL)
+	{
+		while (l < size)
+		{
+			action(array[l]);
+			l++;
+		}
+	}
 
 }
