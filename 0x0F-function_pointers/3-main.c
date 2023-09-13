99d8a62a1378d8766e36ee4ab309f789ad142863
root <root@localhost.localdomain>
2023-09-13 23:03:48 +0100

    Task3: 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: INT
+*/
+int main(int argc, char *argv[])
+{
+    int (*func)(int, int);
+    int a, b;
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit (98);
+    }
+    func = get_op_func(argv[2]);
+
+    if (func == NULL)
+    {
+        printf("Error\n");
+        exit (99);
+    }
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
+
+    printf("%d\n", func(a, b));
+
+    return (0);
+}
