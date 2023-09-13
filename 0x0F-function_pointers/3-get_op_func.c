b563e64c682bf18e47d1c1bf786ca60b67aba153
root <root@localhost.localdomain>
2023-09-13 23:09:33 +0100

    Task3: 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,7 +7,7 @@
 */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 /**
  * op_sub - subtract a
@@ -17,7 +17,7 @@ int op_add(int a, int b)
 */
 int op_sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
 /**
  * op_mul - multiply a
@@ -27,7 +27,7 @@ int op_sub(int a, int b)
 */
 int op_mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 /**
  * op_div - divide a
@@ -37,12 +37,12 @@ int op_mul(int a, int b)
 */
 int op_div(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    return (a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 /**
  * op_mod - mod
@@ -52,10 +52,10 @@ int op_div(int a, int b)
 */
 int op_mod(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
