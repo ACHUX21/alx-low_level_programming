90b262ef7575e1e69caf86fedfa7083f9bdf9f95
root <root@localhost.localdomain>
2023-09-13 21:49:46 +0100

    Task2: 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,12 +9,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || array == NULL || cmp == NULL)
+	if (size <= 0 || array == ((void *)0) || cmp == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
-    return (-1);
+	
+	return (-1);
 }
