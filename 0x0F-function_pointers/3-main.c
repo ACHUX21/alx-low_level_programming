027537d13ee080c4b5d6ac5bb319cb88b7b35170
root <root@localhost.localdomain>
2023-09-13 23:09:35 +0100

    Task3: 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,25 +7,25 @@
 */
 int main(int argc, char *argv[])
 {
-    int (*func)(int, int);
-    int a, b;
+	int (*func)(int, int);
+	int a, b;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit (98);
-    }
-    func = get_op_func(argv[2]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	func = get_op_func(argv[2]);
 
-    if (func == NULL)
-    {
-        printf("Error\n");
-        exit (99);
-    }
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit (99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-    printf("%d\n", func(a, b));
+	printf("%d\n", func(a, b));
 
-    return (0);
+	return (0);
 }
