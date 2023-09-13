ff556b5cce2880434ef76ba56951e101d37bcc5e
root <root@localhost.localdomain>
2023-09-13 23:09:37 +0100

    Task3: 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,20 +9,20 @@
 */
 int (*get_op_func(char *s))(int, int)
 {
-        op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+		op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-    for (i = 0; i < 5 ; i++)
-    {
-        if (strcmp(s, ops[i].op) == 0)
-            return ops[i].f;
-    }
-    return (0);
+	for (i = 0; i < 5 ; i++)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return ops[i].f;
+	}
+	return (0);
 }
