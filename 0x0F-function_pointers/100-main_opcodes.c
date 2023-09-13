6d4b1596edfc6bec9c84672802e10554b06539a6
root <root@localhost.localdomain>
2023-09-13 23:53:17 +0100

    Task100: 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -14,22 +14,22 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-    	printf("Error\n");
-        exit(1);
-    }
+		printf("Error\n");
+		exit(1);
+	}
 
 	b = atoi(argv[2 - 1]) - 1;
 
 	if (b <= -1)
 	{
-    	printf("Error\n");
-        exit(2);
-    }
+		printf("Error\n");
+		exit(2);
+	}
 
-    while (--b)
-    {
-        printf("%02hhx%s", *p++, b ? " " : "\n");
-    }
+	while (--b)
+	{
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	}
 
 	return (0);
 }
