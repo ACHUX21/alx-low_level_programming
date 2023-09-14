dba1d6f605df54dafb8d039b002ccee74a35b5fd
root <root@localhost.localdomain>
2023-09-14 20:53:35 +0100

    Task2: 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,31 +1,27 @@
 #include "variadic_functions.h"
+
 /**
- * print_strings - print strings to stdout
- * @separator: separ argument
- * @n: number of strings to print
-*/
+ * print_strings - prints a string.
+ * @separator: separator string
+ * @n: number of characters to print
+ **/
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-    char *s;
+	char *s;
 	va_list args;
-
-	if (separator == NULL)
-	{
-		separator = "";
-	}
-
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-        s = va_arg(args, char *);
-		if (i !=  0)
+		s = va_arg(args, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if ((separator != NULL) && i < (n - 1))
 			printf("%s", separator);
-        if (!s)
-            s = "(nil)";
-		printf("%s", va_arg(args, char *));
 	}
-	va_end(args);
 	printf("\n");
+	va_end(args);
 }
