ed0e0b0aa56fc2910479d70bc8bb6dda717b7c79
root <root@localhost.localdomain>
2023-09-14 20:25:40 +0100

    Task1: 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,1 +1,27 @@
 #include "variadic_functions.h"
+/**
+ * print_numbers - print numbers
+ * @separator: the separator
+ * @n: the number of numbers
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i !=  0)
+			printf("%s", separator);
+		printf("%u", va_arg(args, unsigned int));
+
+	}
+	va_end(args);
+
+	printf("\n");
+}
