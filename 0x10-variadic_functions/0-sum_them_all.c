75be77e009f390aae1b43455032d351a1cbb85ce
root <root@localhost.localdomain>
2023-09-14 20:03:59 +0100

    Task0: 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,1 +1,26 @@
 #include "variadic_functions.h"
+/**
+ * sum_them_all - sum them all the variables
+ * @n: number of variables
+ * @...: int
+ * Return: INT
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
+	int sum = 0;
+
+	if (n <= 0)
+		return (0);
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n ; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+
+	return (sum);
+}
