049fb93c2425749dd0ed55d6886e6d74af4eff9f
root <root@localhost.localdomain>
2023-09-21 19:03:46 +0100

    Task0: 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,14 @@ size_t print_list(const list_t *h)
 const list_t *ptr = h;
 size_t lenght = 0;
 
-    while (ptr != NULL)
-    {
-        if (ptr->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", ptr->len, ptr->str);
-        lenght++;
-        ptr = ptr->next;
-    }
-    return (lenght);
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", ptr->len, ptr->str);
+		lenght++;
+		ptr = ptr->next;
+	}
+	return (lenght);
 }
