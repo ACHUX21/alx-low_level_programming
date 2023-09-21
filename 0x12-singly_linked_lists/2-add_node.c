36aee682d5bd8da14a985e6cb8c8ff0e2df28078
root <root@localhost.localdomain>
2023-09-21 19:03:48 +0100

    Task2: 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,24 +10,24 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *haruma = malloc(sizeof(list_t));
+	list_t *haruma = malloc(sizeof(list_t));
 
-    if (!haruma)
-        return (NULL);
+	if (!haruma)
+		return (NULL);
 
-    haruma->str = strdup(str);
+	haruma->str = strdup(str);
 
-    if (haruma->str == NULL)
-    {
-        free(haruma);
-        return (NULL);
-    }
-    haruma->len = strlen(str);
-    haruma->next = *head;
-    *head = haruma;
+	if (haruma->str == NULL)
+	{
+		free(haruma);
+		return (NULL);
+	}
+	haruma->len = strlen(str);
+	haruma->next = *head;
+	*head = haruma;
 
 
-    return (haruma);
+	return (haruma);
 }
 /**
  * main - check the code
@@ -36,37 +36,38 @@ list_t *add_node_end(list_t **head, const char *str)
  */
 int main(void)
 {
-    list_t *head;
+	list_t *head;
 
-    head = NULL;
-    add_node_end(&head, "Anne");
-    add_node_end(&head, "Colton");
-    add_node_end(&head, "Corbin");
-    add_node_end(&head, "Daniel");
-    add_node_end(&head, "Danton");
-    add_node_end(&head, "David");
-    add_node_end(&head, "Gary");
-    add_node_end(&head, "Holden");
-    add_node_end(&head, "Ian");
-    add_node_end(&head, "Ian");
-    add_node_end(&head, "Jay");
-    add_node_end(&head, "Jennie");
-    add_node_end(&head, "Jimmy");
-    add_node_end(&head, "Justin");
-    add_node_end(&head, "Kalson");
-    add_node_end(&head, "Kina");
-    add_node_end(&head, "Matthew");
-    add_node_end(&head, "Max");
-    add_node_end(&head, "Michael");
-    add_node_end(&head, "Ntuj");
-    add_node_end(&head, "Philip");
-    add_node_end(&head, "Richard");
-    add_node_end(&head, "Samantha");
-    add_node_end(&head, "Stuart");
-    add_node_end(&head, "Swati");
-    add_node_end(&head, "Timothy");
-    add_node_end(&head, "Victor");
-    add_node_end(&head, "Walton");
-    print_list(head);
-    return (0);
+	head = NULL;
+	add_node_end(&head, "Anne");
+	add_node_end(&head, "Colton");
+	add_node_end(&head, "Corbin");
+	add_node_end(&head, "Daniel");
+	add_node_end(&head, "Danton");
+	add_node_end(&head, "David");
+	add_node_end(&head, "Gary");
+	add_node_end(&head, "Holden");
+	add_node_end(&head, "Ian");
+	add_node_end(&head, "Ian");
+	add_node_end(&head, "Jay");
+	add_node_end(&head, "Jennie");
+	add_node_end(&head, "Jimmy");
+	add_node_end(&head, "Justin");
+	add_node_end(&head, "Kalson");
+	add_node_end(&head, "Kina");
+	add_node_end(&head, "Matthew");
+	add_node_end(&head, "Max");
+	add_node_end(&head, "Michael");
+	add_node_end(&head, "Ntuj");
+	add_node_end(&head, "Philip");
+	add_node_end(&head, "Richard");
+	add_node_end(&head, "Samantha");
+	add_node_end(&head, "Stuart");
+	add_node_end(&head, "Swati");
+	add_node_end(&head, "Timothy");
+	add_node_end(&head, "Victor");
+	add_node_end(&head, "Walton");
+	print_list(head);
+	return (0);
 }
+
