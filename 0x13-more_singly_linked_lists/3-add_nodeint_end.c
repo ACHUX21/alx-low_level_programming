57a0d9106b3f99fc6c661c6589797032e0163a96
achux21 <1337.achraf@gmail.com>
2023-09-26 21:56:29 -0500

    TASK-3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,3 +13,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (!head)
 		return (NULL);
 
+	harrumma = malloc(sizeof(listint_t));
+	if (!harrumma)
+		return (NULL);
+
+	harrumma->n = n;
