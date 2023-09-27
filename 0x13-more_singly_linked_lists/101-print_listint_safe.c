ac70a59a55ef5490db69f9ef94902ae57d4e10be
achux21 <1337.achraf@gmail.com>
2023-09-26 21:51:32 -0500

    TASK-101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,3 +3,8 @@
 
 size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
+
+/**
+ * looped_listint_len - Counts
+ * @head: A pointer
+ * Return: If the list is not looped - 0.
