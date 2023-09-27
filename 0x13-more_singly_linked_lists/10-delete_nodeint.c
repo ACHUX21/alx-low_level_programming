4c453e2876574358e655af7fa95495a1822cfab5
achux21 <1337.achraf@gmail.com>
2023-09-26 21:38:38 -0500

    TASK-10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,10 +1,15 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes 
+ * delete_nodeint_at_index - Deletes
  * @head: Pointer
- * @index: Index 
+ * @index: Index
  * Return: 1 or -1
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *harruma = *head;
+	listint_t *achux = NULL;
+	unsigned int i = 0;
+
+	if (!*head)
