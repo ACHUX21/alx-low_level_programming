1d47e0b66300063e2f55a665c3de5edf80fd9483
achux21 <1337.achraf@gmail.com>
2023-09-26 21:55:07 -0500

    TASK-2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,3 +3,8 @@
  * add_nodeint - Adds a new node
  * @head: Pointer to a pointer
  * @n: Int
+ * Return: Pointer to the newly added node (the head of the list).
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *harruma = malloc(sizeof(listint_t));
