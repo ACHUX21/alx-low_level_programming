e78857ea294484cd907af43bf3b674db0617e6c0
achux21 <1337.achraf@gmail.com>
2023-09-26 21:48:05 -0500

    TASK-1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,3 +3,8 @@
 /**
  * listint_len - Counts the number of elements
  * @h: Pointer to the head of the list.
+ * Return: int
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *haruma = h;
