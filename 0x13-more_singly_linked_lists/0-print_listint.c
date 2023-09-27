32618474cdc26ada1bca6189a065cad51f0cd9bd
achux21 <1337.achraf@gmail.com>
2023-09-26 21:57:30 -0500

    TASK-0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,17 +3,3 @@
  * print_listint - Prints all elements
  * @h: Pointer
  * Return: Number of nodes in the list.
- */
-size_t print_listint(const listint_t *h)
-{
-	const listint_t *harruma = h;
-	int i = 0;
-
-	while (harruma)
-	{
-		printf("%d\n", harruma->n);
-		harruma = harruma->next;
-		i++;
-	}
-	return (i);
-}
