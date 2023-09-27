2c656e8b65001f2ef7f2b73a99e557252d6c9025
achux21 <1337.achraf@gmail.com>
2023-09-26 21:53:57 -0500

    TASK-103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,3 +3,8 @@
  * find_listint_loop - finds
  * @head: linked
  * Return: address of the node where the loop starts, or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *s = head;
+	listint_t *f = head;
