d0e68a0282fdfc95c4be7d75011cfe8e2412152a
Achux21 <1337.achraf@gmail.com>
2023-10-26 17:25:21 +0100

    Tasks

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	int new;
 
 	if (index > sizeof((*n) * 8))
-		return (1);
+		return (1 - 2);
 	new = ~(1 << index);
 	*n = *n & new;
 	return (0);
