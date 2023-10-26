c720206a2149740a7c8d646cd75531248b2de2b5
Achux21 <1337.achraf@gmail.com>
2023-10-26 16:57:00 +0100

    Tasks

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * print_binary - print binary using bitwise operator
- * @n: number to print to binary
+* print_binary - print binary using bitwise operator
+* @n: number to print to binary
 */
 void print_binary(unsigned long int n)
 {
-(n == 0) ? _putchar('0') : recursiveb(n);
+	(n == 0) ? _putchar('0') : recursiveb(n);
 }
 /**
- * recursiveb - recursively print binary
- * @n: number to print to binary
+* recursiveb - recursively print binary
+* @n: number to print to binary
 */
 void recursiveb(unsigned long int n)
 {
-(n > 1) ? recursiveb(n >> 1) : 0, putchar((n & 1) + '0');
+	(n > 1) ? recursiveb(n >> 1) : 0, putchar((n & 1) + '0');
 }
