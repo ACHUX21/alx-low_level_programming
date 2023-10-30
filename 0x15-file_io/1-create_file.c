70ac0c7f9c34b8586d3aaec4f8b997ef6854b75f
Achux21 <1337.achraf@gmail.com>
2023-10-30 23:11:11 +0100

    TASK1: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,6 @@
 #include "main.h"
-#include <unistd.h>
-#include <fcntl.h>
 /**
  * create_file - create a file
- *
  * @filename: The name that the new created file will had
  * @text_content: String the will be written to @filename file
  * Return: 1 on success, -1 on failure
@@ -17,14 +14,10 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
 	if (fd == -1 || filename == NULL)
-	{
 		return (-1);
-	}
 
 	if (text_content == NULL)
-	{
 		return (1);
-	}
 
 	while (*(text_content + i))
 		i++;
@@ -32,9 +25,8 @@ int create_file(const char *filename, char *text_content)
 	size = write(fd, text_content, i);
 
 	if (size == -1)
-	{
 		return (-1);
-	}
+
 	close(fd);
 	return (1);
 }
