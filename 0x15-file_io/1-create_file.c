814dc8b93979c4b914e347ddd484436a1cb2c955
Achux21 <1337.achraf@gmail.com>
2023-10-30 23:05:06 +0100

    TASK1: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,53 @@
 #include "main.h"
+
 /**
-* create_file - create a file
-* @filename: the name of the file
-* @text_content: the content of the file
-* Return: status code
-*/
+ * create_file - Creates or truncates a file and writes text to it.
+ * @filename: The name of the file to create or truncate.
+ * @text_content: The text to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	ssize_t size;
-	int len = 0;
-
-	while (text_content[len] != '\0')
-		len++;
-
-	if (!text_content)
-		text_content = "";
+	int fp = -1;
+	ssize_t written = 0;
 
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file == -1)
-		return (-1);
-if (text_content)
-{
-	size = write(file, text_content, len);
+	if (!text_content)
+		text_content = "";
 
-	if (size == -1)
-	{
-		close(file);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fp < 0)
 		return (-1);
+
+	if (text_content)
+	{
+		written = write(fp, text_content, _strlen(text_content));
+		if (written < 0)
+		{
+			close(fp);
+			return (-1);
+		}
 	}
-}
-	close(file);
+	close(fp);
 
 	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string.
+ * Return: length of @s.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
 }
