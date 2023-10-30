431ceca55ea7a79439e214bc1e07766aed7e44f0
Achux21 <1337.achraf@gmail.com>
2023-10-31 00:47:02 +0100

    TASK3: 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,47 +1,11 @@
 #include "main.h"
 /**
-* error98 - error
-* @filename: the error filename
-*/
-void error98(char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-	exit(98);
-}
-/**
-* error99 - error message from error message
-* @filename: the error message
-*/
-void error99(char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-	exit(99);
-}
-/**
-* close_err - close the error file
-* @file_from: the error file
-* @file_to: the error file
-*/
-void close_err(int file_from, int file_to)
-{
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
-}
-/**
 * main - main function for the application
-* @argc: number of arguments
-* @argv: command line arguments
+* @ac: number of arguments
+* @av: command line arguments
 * Return: number of arguments
 */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
 	ssize_t file_to, file_from;
 	int cto, cfrom;
@@ -50,25 +14,23 @@ int main(int argc, char **argv)
 
 	size_t bytes_read;
 
-	if (argc != 3)
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY);
+	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
-		error98(argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
-		error99(argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+
 
 	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
-	{
 		write(file_to, buffer, bytes_read);
-		if (file_from != file_to)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
+
 	cfrom = close(file_from), cto = close(file_to);
 	if (cto == -1)
 	{
