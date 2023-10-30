3581c16753d6094ab3cc382477942a8b812e6df4
Achux21 <1337.achraf@gmail.com>
2023-10-31 00:03:59 +0100

    TASK3: 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,13 +33,14 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(file_from), close(file_to), exit(99);
 		}
-		if (close(file_from) != -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from), exit(100);
-		}
-		if (close(file_to) != -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_to), exit(100);
-		}
 	}
+	if (close(file_from) != -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from), exit(100);
+	}
+	if (close(file_to) != -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_to), exit(100);
+	}
+	return (0);
 }
