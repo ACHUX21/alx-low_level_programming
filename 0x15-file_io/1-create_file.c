f21db38a2ead5b8cd42910bae1a7e606bc940fd5
Achux21 <1337.achraf@gmail.com>
2023-10-30 22:51:50 +0100

    TASK1: 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+* create_file - create a file
+* @filename: the name of the file
+* @text_content: the content of the file
+* Return: status code
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t size;
+	int len = 0;
+
+	while (text_content[len] != '\0')
+		len++;
+
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	size = write(file, text_content, len);
+
+	if (size == -1)
+	{
+		close(file);
+		return (-1);
+	}
+	close(file);
+
+	return (1);
+}
